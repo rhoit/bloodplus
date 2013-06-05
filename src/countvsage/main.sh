284297a6dad4b5002b1859fcf85e10ab572b3bbd
rho <rho.rhoit@gmail.com>
2013-06-05 11:45:25 +0545

    fixes

---
+++ src/countvsage/main.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+path="../../data"
+out="../../data-process/countvsgroup.csv"
+rm $out
+
+for i in $path/*; do
+    wc -l $i >> $out
+done
+
+sed -i "s/.csv$//g; s/data/,/g; s/[.\/]//g; s/ //g" $out
+sed -i "s/\(.*\),\(.*\)/\2,\1/" $out
+cat $out
