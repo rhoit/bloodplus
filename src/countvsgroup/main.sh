676ba6b9a0e49fd47a556daad8e2ec6c5afb77ec
rho <rho.rhoit@gmail.com>
2013-06-05 11:36:16 +0545

    update file moved

--- src/countvsgroup/main.sh
+++ src/countvsgroup/main.sh
@@ -8,5 +8,6 @@ for i in $path/*; do
     wc -l $i >> $out
 done
 
-sed -i "s/.csv$//g; s/data/\t/g; s/[.\/]//g; s/ //g" $out
-cat $out
+sed -i "s/.csv$//g; s/data/,/g; s/[.\/]//g; s/ //g" $out
+sed "s/\(.*\),\(.*\)/\2,\1/" $out
+#cat $out
